f75d040ec3a6114a1a15fbeee778b6802eb7628a
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-19 00:10:29 -0300

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { ChakraProvider, Input, Button, Center, Box } from "@chakra-ui/react";
-import { Layout } from "./components/Layout";
-// import styled from 'styled-components'
+// App.tsx
+import { ChakraProvider } from "@chakra-ui/react";
+import { Header } from "./components/Header/Header"; // Ajuste o caminho se necessário
+import { Card } from "./components/FormLogin/Card"; // Certifique-se de que o caminho está correto
 
 function App() {
   return (
     <ChakraProvider>
-      <Box minHeight="100vh" backgroundColor="#f5f1ed" p='25px' display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-        <Box backgroundColor="white" boxShadow="dark-lg"borderRadius='25px' p='15' width="full" maxW="md">
-          <Center> 
-            <h1>Faça o Login</h1>
-          </Center>
-          <Input placeholder="email" type="email" mb="4" />
-          <Input placeholder="password"type="password" mb="4"  />
-          <Center>
-            <Button colorScheme="green" width='100%'>Entrar</Button>
-          </Center>
-        </Box>
-      </Box>
+      <Header />
+      <Card id={1} />
     </ChakraProvider>
   );
 }
