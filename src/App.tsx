e1d739b8b280f14fde5c31584a98bfcbc2ad38e4
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-19 15:08:41 -0300

    app com layout ajustado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 // App.tsx
 import { ChakraProvider } from "@chakra-ui/react";
-import { Header } from "./components/Header/Header"; 
 import { Card } from "./components/FormLogin/Card"; 
+import { Layout } from "./components/Layout";
+import { useState } from "react";
+
+
 
 function App() {
+  const [ value , setValue ] = useState(0)
+
+
   return (
     <ChakraProvider>
-      <Header />
+      <Layout>
       <Card id={1} />
+    </Layout>
     </ChakraProvider>
   );
 }
