648056c46313de94bc5b96989567eb2e9ee833d8
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-19 15:08:04 -0300

    teste unitario

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,14 +1,28 @@
-// CustomButton.test.tsx
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import CustomButton from '../components/FormLogin/CustomButton';  
+import { login } from "./login"
 
-describe('CustomButton', () => {
-    it('deve chamar função onClick quando clicado', () => {
-        const onClick = jest.fn();
-        render(<CustomButton onClick={onClick}>Entrar</CustomButton>);
+describe('login', () => {
 
-        fireEvent.click(screen.getByText('Entrar'));
-        expect(onClick).toHaveBeenCalled();
-    });
-});
+    const mockAlert = jest.fn()
+    window.alert = mockAlert
+
+    const mockEmail = 'thi@dio.com'
+
+
+    it('Deve exibir um alert com boas vindas caso o email seja valido', async () => {
+       await  login(mockEmail)
+        expect(mockAlert).toHaveBeenCalledWith(`Bem vindo(a) ${mockEmail}!`)
+    })
+
+    it('nao deve exibir a mensagem de boas vindas sem o email' , async() => {
+        await login(mockEmail)
+        expect(mockAlert).not.toHaveBeenCalledWith('Bem vinda!')
+
+    })
+
+    it('deve exibir um erro caso o email seja invalido' , async() => {
+        await login('email@invalido.com')
+        expect(mockAlert).toHaveBeenCalledWith('Email invalido')
+    
+
+    })
+})
