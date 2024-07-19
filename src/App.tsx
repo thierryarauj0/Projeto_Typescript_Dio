8eb7f70a4ff326783a21747ff77008d8ad9d5aca
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-18 22:39:15 -0300

    ada

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Input, Button, Center, Box } from "@chakra-ui/react";
+import { Layout } from "./components/Layout";
+// import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box minHeight="100vh" backgroundColor="#9413dc" padding='25px'>
+        <Box backgroundColor="white" borderRadius='25px' paddingLeft='15'>
+          <Center> 
+            <h1>Faça o Login</h1>
+           </Center>
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Center>
+            <Button colorScheme="green">Entrar</Button>
+          </Center>
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css' 
+
+export const Header = () => {
+  return(
+
+        <div className='header'>
+        Thierry Bank
+        </div>
+  
+    )
+}
