899485acbc9c5fe5da3fe8aad6c3fcfdb46ceab8
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-19 15:09:36 -0300

    login teste feito

--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,4 +1,12 @@
+import { api } from "../api";
+
 // services/login.js
-export const login = (): void => {
-    alert('Bem-vindo!');
+export const login =  async (email: string): Promise<void> => {
+    const data: any = await api
+
+if( email !== data.email){
+    return alert ('Email invalido')
+}
+
+    alert(`Bem vindo(a) ${email}!`);
 };
