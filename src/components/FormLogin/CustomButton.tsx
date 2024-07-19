27394d867680f8cecbcc7674f2b07b0cb7a03248
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-19 15:09:09 -0300

    button do card login feito

--- src/components/FormLogin/CustomButton.tsx
+++ src/components/FormLogin/CustomButton.tsx
@@ -1,9 +1,9 @@
 // CustomButton.tsx
 import { Button, ButtonProps } from "@chakra-ui/react";
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
 interface CustomButtonProps extends ButtonProps {
-  onClick: () => void;  
+  onClick: MouseEventHandler
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({ onClick, children, ...props }) => {
