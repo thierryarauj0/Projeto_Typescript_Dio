3e9e1673fc3b61227134ca190007b743bd9e2537
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 11:42:54 -0300

    card children

--- src/components/FormLogin/Card.tsx
+++ src/components/FormLogin/Card.tsx
@@ -1,16 +1,11 @@
 // Card.tsx
-import { Box, Center, Input } from "@chakra-ui/react";
-import CustomButton from "./CustomButton";
-import { login } from "../../services/login";
-import { useState } from "react";
+import { Box } from "@chakra-ui/react";
 
-interface CardProps {
-  id: number;
-}
 
-export const Card: React.FC<CardProps> = ({ id }) => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+
+
+export const Card = ({children}: any) => {
+
 
   return (
     <Box
@@ -22,42 +17,10 @@ export const Card: React.FC<CardProps> = ({ id }) => {
       justifyContent="center"
       alignItems="center"
     >
-      <Box
-        backgroundColor="white"
-        boxShadow="dark-lg"
-        borderRadius="25px"
-        p="15"
-        width="full"
-        maxW="md"
-      >
-        <Center>
-          <h1>Faça o Login</h1>
-        </Center>
-
-        <Input
-          placeholder="email"
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          mb="4"
-        />
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          mb="4"
-        />
-        <Center>
-          <CustomButton
-            onClick={() => login(email)}
-            colorScheme="green"
-            width="100%"
-          >
-            Entrar
-          </CustomButton>
-        </Center>
-      </Box>
+     
+        {children}
+        
+    
     </Box>
   );
 };
