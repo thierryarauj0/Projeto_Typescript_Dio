d101c9f2491d979e642606354cdc11ec99de296a
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 02:05:18 -0300

    card

--- src/components/FormLogin/Card.tsx
+++ src/components/FormLogin/Card.tsx
@@ -1,58 +1,63 @@
 // Card.tsx
 import { Box, Center, Input } from "@chakra-ui/react";
-import CustomButton from "./CustomButton"; 
-import { login } from "../../services/login"; 
-import { useEffect, useState } from "react";
-import {api } from '../../api'
-
+import CustomButton from "./CustomButton";
+import { login } from "../../services/login";
+import { useState } from "react";
 
 interface CardProps {
   id: number;
 }
 
-interface UserData {
-  email : string,
-  password: string,
-  name: string
-}
-
 export const Card: React.FC<CardProps> = ({ id }) => {
-  const [ userData, setUserData ] = useState<null | UserData>()
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');  
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-  useEffect(() => {
-    const getData = async () => {
-      const data: any | UserData = await api
-      setUserData(data)
-    }
-    getData()
-  } , [])
-
-
- 
   return (
-    <Box minHeight="100vh" backgroundColor="#f5f1ed" p='25px' display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-      <Box backgroundColor="white" boxShadow="dark-lg" borderRadius='25px' p='15' width="full" maxW="md">
-        {/* {
-        userData === null || userData === undefined ? 
-        <h1>Loading...</h1> :
-        <h1>Informaçoes Carregadas:</h1>
-        }         */}
-     
-        <Center> 
+    <Box
+      minHeight="100vh"
+      backgroundColor="#f5f1ed"
+      p="25px"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        backgroundColor="white"
+        boxShadow="dark-lg"
+        borderRadius="25px"
+        p="15"
+        width="full"
+        maxW="md"
+      >
+        <Center>
           <h1>Faça o Login</h1>
         </Center>
-         <p>{userData?.name}</p> 
-        <Input placeholder="email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} mb="4" />
-        <Input placeholder="password" type="password" value={password} onChange={(event) => setPassword(event.target.value)} mb="4" />
+
+        <Input
+          placeholder="email"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          mb="4"
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          mb="4"
+        />
         <Center>
-          <CustomButton onClick={() => login(email)} colorScheme="green" width='100%'>
+          <CustomButton
+            onClick={() => login(email)}
+            colorScheme="green"
+            width="100%"
+          >
             Entrar
           </CustomButton>
         </Center>
       </Box>
     </Box>
-    
   );
 };
