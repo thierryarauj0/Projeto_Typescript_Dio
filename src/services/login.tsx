a14f4fe6216bc674c8b3daf697c66ff0326193fc
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 13:54:41 -0300

    login

--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,12 +1,20 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useContext } from "react";
 import { api } from "../api";
+import { AppContext } from "../components/AppContext";
+import { useNavigate } from "react-router-dom";
 
 // services/login.js
 export const login =  async (email: string): Promise<void> => {
-    const data: any = await api
+   const {setIsLoggedIn} = useContext(AppContext)
+   const navigate = useNavigate()
+   console.log(setIsLoggedIn) 
+   const data: any = await api
 
 if( email !== data.email){
     return alert ('Email invalido')
 }
-
+    setIsLoggedIn(true)
+    navigate(`/${data.id}`)
     alert(`Bem vindo(a) ${email}!`);
 };
