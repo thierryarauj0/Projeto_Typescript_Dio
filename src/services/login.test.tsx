2f94ff13d36e40a26e631bfa34d777958d1aa725
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 14:22:01 -0300

    teste

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,19 +1,17 @@
 import { login } from "./login"
 
-const mockSetIsLoggedIn = jest.fn()
-const mockNavigate = jest.fn()
 
-jest.mock('react', () => ({
-    ...jest.requireActual('react'), 
-    useContext: () => ({
-        setIsLoggedIn: mockSetIsLoggedIn
-    })
-}))
+// jest.mock('react', () => ({
+//     ...jest.requireActual('react'), 
+//     useContext: () => ({
+//         setIsLoggedIn: mockSetIsLoggedIn
+//     })
+// }))
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'), 
-    useNavigate: () => mockNavigate
-}))
+// jest.mock('react-router-dom', () => ({
+//     ...jest.requireActual('react-router-dom'), 
+//     useNavigate: () => mockNavigate
+// }))
 
 
 describe('login', () => {
@@ -22,18 +20,15 @@ describe('login', () => {
 
 
     it('Deve exibir um alert com boas vindas caso o email seja valido', async () => {
-       await  login(mockEmail)
-       expect(mockSetIsLoggedIn).toHaveBeenCalledWith(true)
-       expect(mockNavigate).toHaveBeenCalledWith('/1')
+       const response = await login(mockEmail)
+       expect(response).toBeTruthy()
+       
     })
 
    
 
     it('deve exibir um erro caso o email seja invalido' , async() => {
-        await login('email@invalido.com')
-       expect(mockSetIsLoggedIn).not.toHaveBeenCalledWith()
-       expect(mockNavigate).not.toHaveBeenCalled()
-    
-
+        const response = await login('email@invalido.com')
+        expect(response).toBeFalsy()
     })
 })
