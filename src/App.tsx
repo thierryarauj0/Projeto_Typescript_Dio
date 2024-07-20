c24eb9589f9586be195950d226d701f44d1a2b5a
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 11:42:32 -0300

    new route

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ import Home from "../src/pages/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Conta from "./pages/Conta";
 import ContaInfo from "./pages/ContaInfo";
+import {  AppContextProvider } from "./components/AppContext";
+
+
 
 function App() {
   return (
     <BrowserRouter>
-      <ChakraProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/conta/:id" element={<Conta />} />
-            <Route path="/infoconta" element={<ContaInfo />} />
-
-          </Routes>
-        </Layout>
-      </ChakraProvider>
+      <AppContextProvider>
+        <ChakraProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/conta/:id" element={<Conta />} />
+              <Route path="/infoconta" element={<ContaInfo />} />
+            </Routes>
+          </Layout>
+        </ChakraProvider>
+      </AppContextProvider>
     </BrowserRouter>
   );
 }
