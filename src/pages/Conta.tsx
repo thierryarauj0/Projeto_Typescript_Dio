89271972949bd86de4ca6d0219954e97d1dec1a1
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 02:31:44 -0300

    new logisct

--- src/pages/Conta.tsx
+++ src/pages/Conta.tsx
@@ -2,12 +2,14 @@ import { Center, SimpleGrid, Spinner } from "@chakra-ui/react";
 import CardInfo from "../components/Conta/Cardinfo";
 import { useState, useEffect } from "react";
 import { api } from "../api";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface UserData {
   email: string;
   password: string;
   name: string;
   balance: number;
+  id:string;
 }
 const Conta = () => {
   const [userData, setUserData] = useState<null | UserData>();
@@ -21,30 +23,34 @@ const Conta = () => {
     getData();
   }, []);
 
-  console.log(userData)
 
-  const actualData = new Date()
-  console.log(actualData.toDateString())
+  const actualData = new Date();
 
+  const {id} = useParams()
+  const navigate = useNavigate()
+  if( userData && id !== userData.id){
+    navigate('/')
+  }
 
   return (
     <Center>
       <SimpleGrid columns={2} spacing={8} paddingTop={16}>
-      {
-            userData === undefined || userData === null ?
-            (
-                <Center>
-                    <Spinner size='xl'/>
-                </Center>
-            ) :
-            (
-       <>
-        <CardInfo mainContent={`Bem vindo(a) ${userData?.name}`} content={`${actualData.getDay()}/${actualData.getMonth()}/${actualData.getFullYear()} ${actualData.getHours()}:${actualData.getMinutes()}`}/>         
-        <CardInfo mainContent={"Saldo:"} content={`R$${userData?.balance}`}  />
-
-       </>
-            )
-        }
+        {userData === undefined || userData === null ? (
+          <Center>
+            <Spinner size="xl" />
+          </Center>
+        ) : (
+          <>
+            <CardInfo
+              mainContent={`Bem vindo(a) ${userData?.name}`}
+              content={`${actualData.getDay()}/${actualData.getMonth()}/${actualData.getFullYear()} ${actualData.getHours()}:${actualData.getMinutes()}`}
+            />
+            <CardInfo
+              mainContent={"Saldo:"}
+              content={`R$${userData?.balance}`}
+            />
+          </>
+        )}
       </SimpleGrid>
     </Center>
   );
