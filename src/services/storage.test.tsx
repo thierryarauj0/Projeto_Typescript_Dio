5d358dcbf7797a22bfb08c59e688c8b2aa2a9a0a
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 01:11:08 -0300

    teste unitario

--- src/services/storage.test.tsx
+++ src/services/storage.test.tsx
@@ -1,20 +1,25 @@
-import { createLocalStorage, getAllLocalStorage } from "./storage"
+import { changeLocalStorage, createLocalStorage, getAllLocalStorage } from "./storage"
 
 const dioBank = {
     login: false
 }    
 
 describe('storage', () => {
+    const MockgetItem = jest.spyOn(Storage.prototype, 'getItem')
+    const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
     
     it('Deve retornar o objeto no localStorage com a chave diobank', () => {
-        const MockgetItem = jest.spyOn(Storage.prototype, 'getItem')
         getAllLocalStorage()
         expect(MockgetItem).toHaveBeenCalledWith('diobank')
     })   
     
     it('Deve criar o objeto no localStorag', () => {
-        const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
         createLocalStorage()
+        expect(mockSetItem).toHaveBeenCalledWith('diobank', JSON.stringify(dioBank))
+    })
+
+    it('deve alterar o valor do objeto no localStorage', () => {
+        changeLocalStorage(dioBank)
         expect(mockSetItem).toHaveBeenCalledWith('diobank', JSON.stringify(dioBank))
     })
 })
