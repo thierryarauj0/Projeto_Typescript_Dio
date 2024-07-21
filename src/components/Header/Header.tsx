f826539ea4e9864efa42e1d098e9794107065827
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 10:04:45 -0300

    button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center, Flex, Spacer, Text } from "@chakra-ui/react";
+import {  Button, Center, Flex,  Text } from "@chakra-ui/react";
 import { useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AppContext } from "../AppContext";
@@ -6,44 +6,39 @@ import { AppContext } from "../AppContext";
 export const Header = () => {
   const { isLoggedIn, setIsLoggedIn } = useContext(AppContext);
   const navigate = useNavigate();
-  const location = useLocation(); // Adiciona o useLocation para acessar a rota atual
-
+  const location = useLocation(); 
   const logout = () => {
     localStorage.removeItem("user");
     setIsLoggedIn(false);
     navigate("/");
   };
 
-  // Altera a funcionalidade dependendo da página atual
   const handleAccountButton = () => {
     if (location.pathname === "/infoconta") {
-      navigate("/conta/1");  // Se estiver em /infoconta, redireciona para /conta/1
+      navigate("/conta/1");  
     } else {
-      navigate("/infoconta");  // Caso contrário, vai para /infoconta
+      navigate("/infoconta"); 
     }
   };
 
   const accountButtonText = location.pathname === "/infoconta" ? "Voltar" : "Minha Conta";
 
   return (
-    <Flex p={5} background="gray.800">
-      <Box bg="gray.800">
-        <Center>
-          <Text fontSize="3xl" color="white">
-            Thierry Bank
-          </Text>
-        </Center>
-      </Box>
-      {isLoggedIn && (
-        <>
-          <Spacer />
-          <Spacer />
-          <Spacer />
-          <Button size="md" onClick={handleAccountButton}>{accountButtonText}</Button>
-          <Spacer />
-          <Button size="md" onClick={logout}>Sair</Button>
-        </>
-      )}
-    </Flex>
+    <Flex p={5} background="gray.800" alignItems="center" justifyContent="space-between">
+    <Center>
+      <Text fontSize="3xl" color="white">
+        Thierry Bank
+      </Text>
+    </Center>
+    {isLoggedIn && (
+      <Flex>
+        <Button size="md" mr={4} onClick={handleAccountButton}>{accountButtonText}</Button>
+        <Button size="md" onClick={logout}>Sair</Button>
+      </Flex>
+    )}
+  </Flex>
+  
+
+
   );
 };
