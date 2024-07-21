bece684ac544b306886d8274970b07b4ccad0fca
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-20 23:53:56 -0300

    botao de sair quando o user esta logado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,32 @@
-import { Box, Center,  Text } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Spacer, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
-  
+  const { isLoggedIn, setIsLoggedIn } = useContext(AppContext);
 
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    navigate("/");
+  };
   return (
-    <Box bg="gray.800">
-      <Center>
-        <Text fontSize="3xl" color="white">
-          Thierry Bank
-        </Text>
-      </Center>
-      {/* <Button borderRadius={0} bg="#f5f1ed">
-        Sair
-      </Button> */}
-    </Box>
+    <Flex p={5} background="gray.800">
+      <Box bg="gray.800">
+        <Center>
+          <Text fontSize="3xl" color="white">
+            Thierry Bank
+          </Text>
+        </Center>
+      </Box>
+      {isLoggedIn && (
+        <>
+          <Spacer />
+          <Button onClick={() => logout()}>Sair</Button>
+        </>
+      )}
+    </Flex>
   );
 };
