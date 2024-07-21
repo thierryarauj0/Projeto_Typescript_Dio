ec9b8fa354a33c19659a76c5e2cc32fdca16c772
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 01:11:15 -0300

    storage ajustado

--- src/services/storage.tsx
+++ src/services/storage.tsx
@@ -1,12 +1,20 @@
+interface IDioBank{
+    login: boolean
+}
+
 const dioBank = {
     login: false
 }    
 
-export const getAllLocalStorage = () => {
-    return localStorage.getItem('')
+export const getAllLocalStorage = (): string | null => {
+    return localStorage.getItem('diobank')
 
 }
 
 export const createLocalStorage = (): void => {
+    localStorage.setItem('diobank', JSON.stringify(dioBank))
+}
+
+export const changeLocalStorage = (dioBank: IDioBank) => {
     localStorage.setItem('diobank', JSON.stringify(dioBank))
 }
