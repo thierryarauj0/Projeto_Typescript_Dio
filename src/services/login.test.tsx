71183d3de8595b30d8004f211505d5d359cbdb0e
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 02:08:07 -0300

    teste unitario do metodo de login

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -1,24 +1,25 @@
 import { login } from "./login"
 
-
-
-
-
 describe('login', () => {
+    const mockEmail = 'thi@dio.com';
+    const mockPassword = '123';
 
-    const mockEmail = 'thi@dio.com'
+    it('Deve exibir um alert com boas vindas caso o email e a senha sejam válidos', async () => {
+        const response = await login(mockEmail, mockPassword);
+        expect(response).toBeTruthy();
+    });
 
+    it('Deve exibir um erro caso o email ou a senha sejam inválidos', async () => {
+        // Teste com email inválido e senha válida
+        let response = await login('email@invalido.com', mockPassword);
+        expect(response).toBeFalsy();
 
-    it('Deve exibir um alert com boas vindas caso o email seja valido', async () => {
-       const response = await login(mockEmail)
-       expect(response).toBeTruthy()
-       
-    })
+        // Teste com email válido e senha inválida
+        response = await login(mockEmail, 'senhaErrada');
+        expect(response).toBeFalsy();
 
-   
-
-    it('deve exibir um erro caso o email seja invalido' , async() => {
-        const response = await login('email@invalido.com')
-        expect(response).toBeFalsy()
-    })
-})
+        // Teste com ambos inválidos
+        response = await login('email@invalido.com', 'senhaErrada');
+        expect(response).toBeFalsy();
+    });
+});
