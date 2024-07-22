bde0593b7d8acc61016c5c6ada653f52f96aa8c5
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-22 00:19:26 -0300

    nice header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import {  Button, Center, Flex,  Text } from "@chakra-ui/react";
+import { Button, Center, Flex, Text } from "@chakra-ui/react";
 import { useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AppContext } from "../AppContext";
@@ -6,7 +6,8 @@ import { AppContext } from "../AppContext";
 export const Header = () => {
   const { isLoggedIn, setIsLoggedIn } = useContext(AppContext);
   const navigate = useNavigate();
-  const location = useLocation(); 
+  const location = useLocation();
+
   const logout = () => {
     localStorage.removeItem("user");
     setIsLoggedIn(false);
@@ -15,30 +16,30 @@ export const Header = () => {
 
   const handleAccountButton = () => {
     if (location.pathname === "/infoconta") {
-      navigate("/conta/1");  
+      navigate("/conta/1");
     } else {
-      navigate("/infoconta"); 
+      navigate("/infoconta");
     }
   };
 
   const accountButtonText = location.pathname === "/infoconta" ? "Voltar" : "Minha Conta";
 
+  // Mostrar botões apenas se estiver logado e não estiver na página de login
+  const shouldShowButtons = isLoggedIn && location.pathname !== "/";
+
   return (
     <Flex p={5} background="gray.800" alignItems="center" justifyContent="space-between">
-    <Center>
-      <Text fontSize="3xl" color="white">
-        Thierry Bank
-      </Text>
-    </Center>
-    {isLoggedIn && (
-      <Flex>
-        <Button size="md" mr={4} onClick={handleAccountButton}>{accountButtonText}</Button>
-        <Button size="md" onClick={logout}>Sair</Button>
-      </Flex>
-    )}
-  </Flex>
-  
-
-
+      <Center>
+        <Text fontSize="3xl" color="white">
+          Thierry Bank
+        </Text>
+      </Center>
+      {shouldShowButtons && (
+        <Flex>
+          <Button size="md" mr={4} onClick={handleAccountButton}>{accountButtonText}</Button>
+          <Button size="md" onClick={logout}>Sair</Button>
+        </Flex>
+      )}
+    </Flex>
   );
 };
